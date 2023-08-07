0d7bae28504d6cc33f1275899705f0726cc281af
Md Anik Hossain <anikh499@gmail.com>
2023-08-07 18:37:40 +0600

    Assignment done

--- models/auth.model.js
+++ models/auth.model.js
@@ -2,6 +2,9 @@
 const mongoose = require('mongoose')
 
 const authSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
     username: {
         type: String,
         require: true,
